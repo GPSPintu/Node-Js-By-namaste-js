7feb9c34e7a60640ae33bc492ff41479bb1168af
GPSPintu <pintugps.in@gmail.com>
2025-10-19 22:34:09 +0530

    npm

---
+++ Chapter 13 - Creating a database & mongodb/database.js
@@ -0,0 +1,8 @@
+//GO TO MONGODB WEBSITE AND CREATE AN ACCOUNT
+// CREATE A FREE M0 CLUSTER
+//create a users 
+//GET THE CONECTION STRING AND REPLACE <PASSWORD> WITH YOUR PASSWORD
+//REPLACE myFirstDatabase WITH YOUR DATABASE NAM
+
+require("mongodb");
+const URI = "mongodb+srv://pintugps:Pintu321@@namastenodedb.els48in.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNodeDb";
